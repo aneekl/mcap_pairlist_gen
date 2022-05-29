438b49495536b1434545a98f2e71300554b3911b
SPP User <aneekl014@hotmail.com>
2022-05-29 23:47:28 +0700

    hardcode to make output

--- gen.sh
+++ gen.sh
@@ -1,1 +1,1 @@
- python3 market_cap_pairlist.py -n $1 --stake_currency $2 --blacklist user_data/blacklist-binance.json
+python3 market_cap_pairlist.py -n $1 --stake_currency $2 --blacklist user_data/blacklist-$3.json > pairlist/$3-$2-$1.json
