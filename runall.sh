7d0c406a31838c1045e6f96b35c9d9cd4dd7e314
SPP User <aneekl014@hotmail.com>
2022-05-30 00:42:57 +0700

    Create runall.sh

---
+++ runall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+current_pwd=`pwd`
+#coins=80
+
+${current_pwd}/gen.sh $1 usd ftx && sleep 2
+${current_pwd}/gen.sh $1 usdt ftx  && sleep 2
+${current_pwd}/gen.sh $1 usdt binance2 && sleep 2
+${current_pwd}/gen.sh $1 busd binance2  && sleep 2
+${current_pwd}/gen.sh $1 usdt gateio && sleep 2
+${current_pwd}/gen.sh $1 usdt kucoin && sleep 2
+${current_pwd}/gen.sh $1 usdt okx && sleep 2
+${current_pwd}/gen.sh $1 usdt huobi && sleep 2
+${current_pwd}/gen.sh $1 usdt bybit && sleep 2
+
+echo "done"
